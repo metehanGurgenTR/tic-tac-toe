a97e3580de56df34da3d2ae5dc18f50614943e88
metehanGurgenTR <metehangurgentr@outlook.com>
2025-04-27 22:01:59 -0400

    feat: implement insert letter X, switch the player after each turn/insertion of a letter

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+let player1 = "player1";
+let player2 = "player2";
+let currentPlayer = player1;
+
 addEventListener("click", (event) => {{
   const square = event.target;
-  insertO(square.className);
+  if (currentPlayer === player1) {
+    insertX(square.className);
+  } else {
+    insertO(square.className);
+  }
+  currentPlayer = switchPlayer(currentPlayer);
 }});
 
 function insertO(square) {
@@ -8,4 +17,16 @@ function insertO(square) {
   letter.classList.add("player-o");
   letter.textContent = "O";
   document.querySelector('.'+square).appendChild(letter);
+}
+
+function insertX(square) {
+  const letter = document.createElement("span");
+  letter.classList.add("player-x");
+  letter.textContent = "X";
+  document.querySelector('.'+square).appendChild(letter);
+}
+
+function switchPlayer(currentPlayer) {
+  currentPlayer === player1 ? currentPlayer = player2 : currentPlayer = player1;
+  return currentPlayer;
 }
