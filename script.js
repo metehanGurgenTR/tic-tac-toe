9c82d4817e26e3ffe6dffc572799a18fc45f56d9
metehanGurgenTR <metehangurgentr@outlook.com>
2025-04-28 13:07:06 -0400

    refactor: enhance insert a letter functionality using only one function instead of two, prevent inserting a letter outside the squares, and prevent more than one letter for each square

--- script.js
+++ script.js
@@ -2,28 +2,32 @@ let player1 = "player1";
 let player2 = "player2";
 let currentPlayer = player1;
 
+let set = new Set();
+for (let i = 1; i <= 9; i++) {
+  set.add("square" + i);
+}
+
 addEventListener("click", (event) => {{
   const square = event.target;
-  if (currentPlayer === player1) {
-    insertX(square.className);
-  } else {
-    insertO(square.className);
-  }
+  insertLetter(square.className, currentPlayer);
   currentPlayer = switchPlayer(currentPlayer);
 }});
 
-function insertO(square) {
-  const letter = document.createElement("span");
-  letter.classList.add("player-o");
-  letter.textContent = "O";
-  document.querySelector('.'+square).appendChild(letter);
-}
-
-function insertX(square) {
-  const letter = document.createElement("span");
-  letter.classList.add("player-x");
-  letter.textContent = "X";
-  document.querySelector('.'+square).appendChild(letter);
+function insertLetter(square, currentPlayer) {
+    if (set.has(square)) {
+      const letter = document.createElement("span");
+      if (currentPlayer == player1) {
+        letter.classList.add("player-x");
+        letter.textContent = "X";
+      } else {
+        letter.classList.add("player-o");
+        letter.textContent = "O";
+      }
+      document.querySelector('.'+square).appendChild(letter);
+      document.querySelector('.'+square).classList.add("hasLetter");
+    } else {
+      alert("Must insert a letter into an empty square!");
+    }
 }
 
 function switchPlayer(currentPlayer) {
